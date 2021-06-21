78d7fb9f31e2d00c825acf9e96f976d98671b0d5
Eric Eaton <zaimzet@gmail.com>
2021-06-21 01:53:10 -0700

    Set up build dependency on excludes file & Fix snwprintf
    The cmake build is set up to depend on the excludes file, if it was
    supplied, and some troublesome C standard library functions that
    Irrlicht meddles with using macros are excluded from its output.

--- cmake/unplusplus.cmake
+++ cmake/unplusplus.cmake
@@ -8,12 +8,25 @@ if (NOT add_unplusplus_clib)
         set(clib_name "${clib_name}-clib")
         message("clib name ${clib_name}")
 
+        set(excludes_file_name "")
+        foreach (arg ${ARGN})
+            if (lastarg STREQUAL "--excludes-file")
+                set(excludes_file_name "${arg}")
+            endif()
+            set(lastarg "${arg}")
+        endforeach()
+
+        if(excludes_file_name)
+            cmake_path(ABSOLUTE_PATH excludes_file_name NORMALIZE)
+            message("excludes file: ${excludes_file_name}")
+        endif()
+
         add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${clib_name}.h"
             "${CMAKE_CURRENT_BINARY_DIR}/${clib_name}.cpp"
             COMMAND "${UNPLUSPLUS_EXECUTABLE}"
             -o "${clib_name}" "${header_file}" "--extra-arg-before=-xc++-header" ${ARGN}
             MAIN_DEPENDENCY "${header_file}"
-            DEPENDS unplusplus)
+            DEPENDS unplusplus "${excludes_file_name}")
         add_library("${clib_name}" "${CMAKE_CURRENT_BINARY_DIR}/${clib_name}.cpp")
         target_include_directories("${clib_name}" PUBLIC "${CMAKE_CURRENT_BINARY_DIR}")
         target_link_libraries("${clib_name}" "${cxx_library}")
